bdef0bd2c5449f583b4bc9607c7dfa3d2125a1a9
gauravmishra00 <gauravonbeat@gmail.com>
2026-05-13 21:40:21 +0530

    firebase config

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -10,26 +10,40 @@
 // const auth = getAuth(app);
 
 // export default auth;
+// import { initializeApp, cert } from "firebase-admin/app";
+// import { getAuth } from "firebase-admin/auth";
+
+// const serviceAccount = {
+//   type: process.env.FIREBASE_TYPE,
+//   project_id: process.env.FIREBASE_PROJECT_ID,
+//   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+//   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+//   client_email: process.env.FIREBASE_CLIENT_EMAIL,
+//   client_id: process.env.FIREBASE_CLIENT_ID,
+//   auth_uri: process.env.FIREBASE_AUTH_URI,
+//   token_uri: process.env.FIREBASE_TOKEN_URI,
+//   auth_provider_x509_cert_url:
+//     process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+//   client_x509_cert_url:
+//     process.env.FIREBASE_CLIENT_CERT_URL,
+// };
+
+// const app = initializeApp({
+//   credential: cert(serviceAccount),
+// });
+
+// const auth = getAuth(app);
+
+// export default auth;
+
+
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url:
-    process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url:
-    process.env.FIREBASE_CLIENT_CERT_URL,
-};
+import serviceAccountKey from "/etc/secrets/serviceAccountKey.json" with { type: "json" };
 
 const app = initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert(serviceAccountKey),
 });
 
 const auth = getAuth(app);
